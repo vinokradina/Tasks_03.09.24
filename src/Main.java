fe3327124fea55d3fa9f98d057459b97c035283b
Alinka Vinokradinka <alinaivanova05@gmail.com>
2024-09-04 16:50:39 +0300

    1-9

---
+++ src/Main.java
@@ -0,0 +1,206 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Задача 1
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите целое число: ");
+//        int num = in.nextInt();
+//
+//        if (num % 2 == 0)
+//        {
+//            System.out.println("Число чётное!");
+//        }
+//        else
+//        {
+//            System.out.println("Число нечётное!");
+//        }
+//
+//        in.close();
+
+        //Задача 2
+//       Scanner in = new Scanner(System.in);
+//
+//       System.out.print("Введите 1 число: ");
+//       int num1 = in.nextInt();
+//
+//       System.out.print("Введите 2 число: ");
+//       int num2 = in.nextInt();
+//
+//       System.out.print("Введите 3 число: ");
+//       int num3 = in.nextInt();
+//
+//       if ((num1 < num2) && (num1 < num3))
+//       {
+//           System.out.printf("Число %d является минимальным", num1);
+//       }
+//       else if ((num2 < num1) && (num2 < num3))
+//       {
+//           System.out.printf("Число %d является минимальным", num2);
+//       }
+//       else
+//       {
+//           System.out.printf("Число %d является минимальным", num3);
+//       }
+//
+//       in.close();
+
+        //Задача 3
+//        for (int i = 1; i <= 10; i++)
+//        {
+//            System.out.printf("5 * %d = %d \n", i, 5 * i);
+//        }
+
+        //Задача 4
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите число N: ");
+//        int N = in.nextInt();
+//
+//        int sum = 0;
+//
+//        for (int i = 1; i <= N; i++)
+//        {
+//            sum += i;
+//        }
+//
+//        System.out.printf("Сумма чисел от 1 до %d равна %d", N, sum);
+//
+//        in.close();
+
+        //Задача 5
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите число N: ");
+//        int N = in.nextInt();
+//
+//        int num0 = 0;
+//        int num1 = 1;
+//        int num2;
+//
+//        System.out.printf("Первые %d чисел Фибоначчи: %d %d ", N, num0, num1);
+//
+//        for (int i = 3; i <= N; i++)
+//        {
+//            num2 = num0 + num1;
+//            System.out.print(num2 + " ");
+//            num0 = num1;
+//            num1 = num2;
+//        }
+//
+//        in.close();
+
+        //Задача 6
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите число: ");
+//        int a = in.nextInt();
+//
+//        boolean IsNotSimple = false;
+//        for (int i = 2; i < a; i++)
+//        {
+//            if (a % i == 0)
+//            {
+//                IsNotSimple = true;
+//                break;
+//            }
+//        }
+//
+//        if (IsNotSimple)
+//        {
+//            System.out.printf("Число %d не является простым", a);
+//        }
+//        else
+//        {
+//            System.out.printf("Число %d является простым", a);
+//        }
+//
+//        in.close();
+
+        //Задача 7
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите число N: ");
+//        int N = in.nextInt();
+//
+//        for (int i = N; i >= 1; i--)
+//        {
+//            System.out.print(i + " ");
+//        }
+//
+//        in.close();
+
+        //Задача 8
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите число A: ");
+//        int A = in.nextInt();
+//
+//        System.out.print("Введите число B: ");
+//        int B = in.nextInt();
+//
+//        int sum = 0;
+//
+//        for (int i = A; i <= B; i++)
+//        {
+//            if (i % 2 == 0)
+//            {
+//                sum += i;
+//            }
+//        }
+//
+//        System.out.printf("Сумма всех чётных чисел в диапазоне от %d до %d равна %d", A, B, sum);
+//
+//        in.close();
+
+        //Задача 9
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.print("Введите строку: ");
+//        String line = in.nextLine();
+//
+//        StringBuffer strBuffer = new StringBuffer(line);
+//        strBuffer.reverse();
+//        System.out.println(strBuffer.toString());
+//
+//        in.close();
+
+        //Задача 10
+
+
+        //Задача 11
+
+
+        //Задача 12
+
+
+        //Задача 13
+
+
+        //Задача 14
+
+
+        //Задача 15
+
+
+        //Задача 16
+
+
+        //Задача 17
+
+
+        //Задача 18
+
+
+        //Задача 19
+
+
+        //Задача 20
+
+
+
+    }
+}
